c9798d56ce140c45fbf44e9ac574df549bbdb8f6
Hidemaro Fujinami <guppy4.spurs21@gmail.com>
2020-08-01 17:56:09 +0900

    punとtoioの連携成功

---
+++ Assets/CharaScript.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using toio;
+
+public class CharaScript : MonoBehaviour
+{
+    GameObject gameObject;
+
+    void Start()
+    {
+        gameObject = GameObject.Find("scene");
+        Cube cube = gameObject.GetComponent<SimplePun>().cube;
+        Debug.Log(cube);
+        gameObject.GetComponent<SimplePun>().ReturnAccess();
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.Return)){
+            gameObject.GetComponent<SimplePun>().moveToio();
+        }
+        if (Input.GetKey("up"))
+        {
+            transform.position += transform.forward * 0.05f;
+        }
+        if (Input.GetKey("down"))
+        {
+            transform.position -= transform.forward * 0.05f;
+        }
+        if (Input.GetKey("right"))
+        {
+            transform.Rotate(0, 100 * Time.deltaTime, 0) ;
+        }
+        if (Input.GetKey("left"))
+        {
+            transform.Rotate(0, -100 * Time.deltaTime, 0);
+        }
+    }
+}
--- Assets/SimplePun.cs
+++ Assets/SimplePun.cs
@@ -1,25 +1,61 @@
-﻿using Photon.Pun;
+﻿using UnityEngine;
+using System.Collections;
+using Photon.Pun;
 using Photon.Realtime;
-using UnityEngine;
+using toio;
 
-// MonoBehaviourではなくMonoBehaviourPunCallbacksを継承して、Photonのコールバックを受け取れるようにする
-public class SampleScene : MonoBehaviourPunCallbacks
+public class SimplePun : MonoBehaviourPunCallbacks
 {
-    private void Start() {
-        // PhotonServerSettingsに設定した内容を使ってマスターサーバーへ接続する
+    public Cube cube;
+    public int num = 1;
+
+    async void Start()
+    {
+        //旧バージョンでは引数必須でしたが、PUN2では不要です。
         PhotonNetwork.ConnectUsingSettings();
+
+        if (string.IsNullOrEmpty(PhotonNetwork.NickName))
+        {
+            PhotonNetwork.NickName = "player" + Random.Range(1, 99999);
+        }
+
+        Debug.Log("initialized pun2");
+
+        var peripheral = await new NearestScanner().Scan();
+        cube = await new CubeConnecter().Connect(peripheral);
+
+        Debug.Log("initialized toio");
     }
 
-    // マスターサーバーへの接続が成功した時に呼ばれるコールバック
-    public override void OnConnectedToMaster() {
+    public void ReturnAccess() {
+        Debug.Log("Simple Pun Class Access Succeeded!");
+    }
+
+    void OnGUI()
+    {
+        //ログインの状態を画面上に出力
+        GUILayout.Label(PhotonNetwork.NetworkClientState.ToString());
+    }
+
+
+    //ルームに入室前に呼び出される
+    public override void OnConnectedToMaster()
+    {
         // "room"という名前のルームに参加する（ルームが無ければ作成してから参加する）
         PhotonNetwork.JoinOrCreateRoom("room", new RoomOptions(), TypedLobby.Default);
     }
 
-    // マッチングが成功した時に呼ばれるコールバック
-    public override void OnJoinedRoom() {
-        // マッチング後、ランダムな位置に自分自身のネットワークオブジェクトを生成する
-        var v = new Vector3(Random.Range(-3f, 3f), Random.Range(-3f, 3f));
-        PhotonNetwork.Instantiate("GamePlayer", v, Quaternion.identity);
+    //ルームに入室後に呼び出される
+    public override void OnJoinedRoom()
+    {
+        //キャラクターを生成
+        GameObject chara= PhotonNetwork.Instantiate("chara", new Vector3(0.0f,0.5f,0.0f), Quaternion.identity, 0);
+        //自分だけが操作できるようにスクリプトを有効にする
+        CharaScript charaScript = chara.GetComponent<CharaScript>();
+        charaScript.enabled = true;
     }
+
+    public void moveToio() {
+        cube.Move(50, -50, 200);
+    } 
 }
